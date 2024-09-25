ef2c11fddc972a01a74390819b6502de09c01d3e
JayaKishore535 <jayakishore7077@gmail.com>
2024-09-25 21:06:36 +0530

    second commit

--- src/main/java/com/qrdemo/qrdemo/QrDemoApplication.java
+++ src/main/java/com/qrdemo/qrdemo/QrDemoApplication.java
@@ -3,9 +3,15 @@ package com.qrdemo.qrdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@RestController
 @SpringBootApplication
 public class QrDemoApplication {
 
+	@getmapping ("/message")
+	public String message () {
+		return "qr server has been deployed in azure " ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(QrDemoApplication.class, args);
 	}
